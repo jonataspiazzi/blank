a88b8cc0143df9ad7de03bf842aa522d78d01f16
Jonatas Piazzi <piazzi_7@hotmail.com>
2018-11-09 15:28:48 -0200

    temp

---
+++ src/core/snapshot.js
@@ -0,0 +1,16 @@
+
+class Snapshot {
+  cropSnapshot(boardData) {
+    let minY = 0;
+    let minX = 0;
+    let maxY = boardData.length;
+    let maxX = boardData[0].length;
+
+
+
+
+
+  }
+}
+
+export default Snapshot;
