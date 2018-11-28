7afe51072bcff6af542c1a8a78a41ccb6e1128d9
jonataspiazzi <piazzi_7@hotmail.com>
2018-11-28 21:35:51 -0200

    Start React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-global._babelPolyfill = false;
-import 'babel-polyfill';
-import './index.scss';
-import board from './ui/board';
-import form from './ui/form';
-import game from './core/game';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-const index = () => {
-  console.log('link5 inteligence lab was started');
-  form.load();
-  board.clear();
-  game.reset();
+class App extends Component {
+  render() {
+    return (
+      <p>Hello World!</p>
+    );
+  }
 }
 
-document.addEventListener('DOMContentLoaded', index);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-export default index;
+//const index = () => {
+//  console.log('link5 inteligence lab was started');
+//  form.load();
+//  board.clear();
+//  game.reset();
+//}
+//
+//document.addEventListener('DOMContentLoaded', index);
+//
+//export default index;
+
+//global._babelPolyfill = false;
+//import 'babel-polyfill';
+//import './index.scss';
+//import board from './ui/board';
+//import form from './ui/form';
+//import game from './core/game';
